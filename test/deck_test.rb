5c1a7444607084061d314201e6c4e9063213e855
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-19 11:32:43 -0600

    Adds count method and test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,4 +14,12 @@ class DeckTest < Minitest::Test
     assert_instance_of Deck, deck
   end
 
+  def test_instance_variable_count
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal 3, deck.count
+  end
+
 end
