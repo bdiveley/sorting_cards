e2fb6f383fe193a32b447c971abd49cd51ea0372
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-19 11:59:40 -0600

    Adds and tests instance variables

--- lib/round.rb
+++ lib/round.rb
@@ -1,8 +1,10 @@
 class Round
-  attr_accessor :deck
+  attr_accessor :deck, :guesses, :response
 
   def initialize(deck)
     @deck = deck
+    @guesses = []
   end
+
 
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -16,4 +16,20 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, round
   end
 
+  def test_instance_variable_deck
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+  end
+
+  def test_instance_variable_guesses
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal [], round.guesses
+  end
+
 end
