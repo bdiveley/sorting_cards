cc174758e0d6c9a023e3108871c5415d537ab04f
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-19 10:58:52 -0600

    Finish iteration 1

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,25 +1,26 @@
-require './lib/card'
-
 class Guess
-  attr_accessor :response, :card, :value, :suit
+  attr_accessor :response, :card, :value, :suit, :correct
 
   def initialize(response, card)
     @response = response
     @card = card
+    @correct = nil
   end
 
   def correct?
     if card.value == @response.split[0] && card.suit == @response.split[-1]
-      true
+      @correct = true
     else
-      false
+      @correct = false
     end
   end
 
-  # def feedback
-  #   if guess.correct?
-  #     puts "Correct!"
-  #   end
-  # end
+  def feedback
+    if @correct
+      return "Correct!"
+    else
+      return "Incorrect."
+    end
+  end
 
 end
