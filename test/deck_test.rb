d92291d7b73f479d40faf1a7ac8ef120b0881c34
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-19 11:28:07 -0600

    Adds new deck initialize method and tests

---
+++ test/deck_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+require 'pry'
+
+class DeckTest < Minitest::Test
+
+  def test_instance_of_class
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_instance_of Deck, deck
+  end
+
+end
