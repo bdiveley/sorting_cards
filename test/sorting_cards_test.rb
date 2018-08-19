359a1b03f36908626bac584f274cf3aa24fbd3d9
Bailey Diveley <bailey.diveley@gmail.com>
2018-08-19 08:51:45 -0600

    Test value and suit instance variables

--- test/sorting_cards_test.rb
+++ test/sorting_cards_test.rb
@@ -7,6 +7,16 @@ class CardTest < Minitest::Test
   def test_instance_of_card_class
     card = Card.new("Ace", "Spades")
     assert_instance_of Card, card
-  end 
+  end
+
+  def test_value
+    card = Card.new("Ace", "Spades")
+    assert_equal "Ace", card.value
+  end
+
+  def test_suit
+    card = Card.new("Ace", "Spades")
+    assert_equal "Spades", card.suit 
+  end
 
 end
